0a158b62bf20d3dc36db7ccd98232da0823e2b5e
Colin Craig <73488952+ccraig17@users.noreply.github.com>
2024-09-15 08:54:15 -0400

    Added a Toast Message

--- app/src/main/java/com/craig/test3/MainActivity.java
+++ app/src/main/java/com/craig/test3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.craig.test3;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();;
     }
+
 }
